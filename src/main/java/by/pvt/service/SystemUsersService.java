d4587e201e4c85c80f514d9bf2dc5b231baac079
VladimirKameko <drugs1360@gmail.com>
2019-07-18 22:39:50 +0300

    first commit

---
+++ src/main/java/by/pvt/service/SystemUsersService.java
@@ -0,0 +1,73 @@
+package by.pvt.service;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import by.pvt.dao.SystemUsersMapper;
+import by.pvt.dto.SystemUsers;
+
+
+public class SystemUsersService {
+    private static Logger log = Logger.getLogger(SystemUsersService.class.getName());
+    private SqlSessionFactory sqlSessionFactory;
+
+    public SystemUsersService() {
+        try {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(
+                    Resources.getResourceAsStream("by/pvt/service/mybatis-config.xml")
+            );
+
+
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    public List<SystemUsers> getSystemUsers() {
+        SqlSession session = sqlSessionFactory.openSession();
+        SystemUsersMapper dao =
+                session.getMapper(SystemUsersMapper.class);
+
+        List<SystemUsers> dtoUsers
+                = dao.selectByExample(null);
+
+        session.close();
+        return dtoUsers;
+    }
+
+    public void add(SystemUsers systemUser) {
+        SqlSession session = sqlSessionFactory.openSession();
+        SystemUsersMapper dao = session.getMapper(SystemUsersMapper.class);
+        int result = dao.insert(systemUser);
+        log.info("Added new systenUser with result= " + result);
+        session.commit();
+        session.close();
+
+
+    }
+
+    public static void main(String[] args) {
+
+        SystemUsers systemUsers = new SystemUsers();
+        systemUsers.setId(3);
+        systemUsers.setUsername("User2");
+        systemUsers.setActive(false);
+        systemUsers.setDateofbirth(new Date());
+
+        new SystemUsersService().add(systemUsers);
+
+        SystemUsersService service = new SystemUsersService();
+        service.getSystemUsers()
+                .forEach(user ->
+                        log.info(user.getId() + " " + user.getUsername() + " " + user.getActive() + " " + user.getDateofbirth()));
+
+    }
+}
